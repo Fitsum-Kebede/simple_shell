aa15edfacc5b4d53c9176eac75afcfc8c0fa39e9
fitsum-kebede <fitsuminfome@gmail.com>
2023-05-15 08:56:36 -0700

    Betty.c

---
+++ betty.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - A program that prints a text.
+ *
+ * Return: 0 on sucess
+ */
+
+int main(void)
+{
+	printf("This is a simple_shell group project done by Victor Lenny and Fitsum Kebede");
+	return (0);
+}	
