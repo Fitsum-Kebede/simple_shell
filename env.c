dc88cb7e066c5a12762e2035736dd80f860c6d45
victor346pixel <monicamwendia84@gmail.com>
2023-05-22 12:10:09 -0700

    updated

---
+++ env.c
@@ -0,0 +1,60 @@
+#include "shell.h"
+
+
+/**
+ * _isenv - prints new environment
+ *
+ * Return:  NULL - if an error occurs.
+ *             otherwise -  double pointer to new environment.
+ */
+char **_isenv(void)
+{
+	char **new_environ;
+	size_t size;
+	int index;
+
+	for (size = 0; environ[size]; size++)
+		;
+
+	new_environ = malloc(sizeof(char *) * (size + 1));
+	if (!new_environ)
+		return (NULL);
+
+	for (index = 0; environ[index]; index++)
+	{
+		new_environ[index] = malloc(_strlen(environ[index]) + 1);
+
+		if (!new_environ[index])
+		{
+			for (index--; index >= 0; index--)
+				free(new_environ[index]);
+			free(new_environ);
+			return (NULL);
+		}
+		_strcpy(new_environ[index], environ[index]);
+	}
+	new_environ[index] = NULL;
+
+	return (new_environ);
+}
+
+/**
+ * _getenv - acquire environment variable from PATH.
+ * @var: name of  environment variable to get.
+ *
+ * Return:  NULL - If does not exist.
+ *         Otherwise - pointer to environment variable.
+ */
+char **_getenv(const char *var)
+{
+	int index, len;
+
+	len = _strlen(var);
+	for (index = 0; environ[index]; index++)
+	{
+		if (_strncmp(var, environ[index], len) == 0)
+			return (&environ[index]);
+	}
+
+	return (NULL);
+}
